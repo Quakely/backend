a1875cb0ce76c0146e3fb9de908d9228f080029e
Elie Sfeir <42282499+Internalizable@users.noreply.github.com>
2024-05-06 20:51:50 +0300

    Fix region query.

--- src/modules/earthquake/controllers/earthquake.controller.ts
+++ src/modules/earthquake/controllers/earthquake.controller.ts
@@ -14,7 +14,7 @@ export class EarthquakeController {
         const user = res.locals.user as HydratedDocument<User>;
         const page = req.query.page ? Number(req.query.page) : 1;
         const size = req.query.size ? Number(req.query.size) : 10;
-        const regionType = req.query.region ? req.query.regionType as "local" | "regional" | "global" : "local";
+        const regionType = req.query.region ? req.query.region as "local" | "regional" | "global" : "local";
 
         const earthquakes = await getEarthquakeService()
             .getEarthquakes(user.location_options, regionType, page, size);
